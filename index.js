e2b0d0830ab8627ffc52e985ebec832406543330
Joseph-Hansel <abogejosephhansel@gmail.com>
2025-06-29 01:28:54 +0300

    Finish Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",{ 
+        method : "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept":  'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+            .then(res => res.json())
+            .then((data) => {
+                let div = document.createElement("div")
+                div.textContent = data.id;
+                document.querySelector("body").appendChild(div);
+            })
+            .then(data => {
+                console.log("DATA:", data)
+            })
+            .catch(function (error) {
+                let div = document.createElement("div")
+                div.textContent = error.message;
+                document.querySelector("body").appendChild(div);
+            });
+    
+};
+submitData();
